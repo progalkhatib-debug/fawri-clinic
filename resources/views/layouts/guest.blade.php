e41b35ddf7ca92c994fa08041ce96b40b7565730
Your Name <your-email@example.com>
2026-07-20 20:41:59 +0300

    Add csrf-token to booking page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
     /* ... التنسيقات السابقة ... */
 
